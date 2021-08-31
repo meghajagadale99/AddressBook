ebc3488ac90937503d0256eaff4b4279eb98d7ee
Megha <meghajagadale99@gmail.com>
2021-08-31 12:05:29 +0530

    Added: add new contact in a address book

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Welcome to Address Book program.");
         AddressBook storeValue = new AddressBook("Megha", "Jagadale", "Mumbai", "Maharashtra", "abc99@gmail.in", 400071, 100);
         storeValue.Display();
+        storeValue.add();
     }
 }
