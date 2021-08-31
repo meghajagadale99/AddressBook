04e13a41838d887c9f44af7d7718dd2da0fac1f4
Megha <meghajagadale99@gmail.com>
2021-08-31 11:54:34 +0530

    Added: create a new contact

---
+++ src/com/addressbook/Main.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book program.");
+        AddressBook storeValue = new AddressBook("Megha", "Jagadale", "Mumbai", "Maharashtra", "abc99@gmail.in", 400071, 100);
+        storeValue.Display();
+    }
+}
